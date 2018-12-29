e9925d2c8a52f311efe4cddbd8df6c78d22aa7a6
Jonathan Llerandi <jonathanllerandi@gmail.com>
2018-12-29 01:32:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var name = "Susan"
 var height = "70"
-
+if (height === "70") {
+    console.log("The height is correct")
+} else {
+    console.log("Expected to be 70")
+}
+}
 // Don't worry about this
 module.exports = { name, height
 }
